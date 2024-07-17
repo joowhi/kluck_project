1b9f7eb8f043d57d6f649179eab1c76addcdf99d
DESKTOP-9BFGVDK\com <mjk6984@gmail.com>
2024-07-17 14:21:04 +0900

    [BE] Fix: ios 푸시 알림 트래픽 분산 코드 추가

--- kluck_notifications/push_scheduler.py
+++ kluck_notifications/push_scheduler.py
@@ -95,8 +95,20 @@ def send_push_android():
 def send_push_ios():
     try:
         # DB에서 IOS 디바이스 토큰 가져오기
-        ios_registration_tokens = list(DeviceToken.objects.filter(device_os='ios').values_list('token', flat=True))
+        ios_registration_tokens_all = list(DeviceToken.objects.filter(device_os='ios').values_list('token', flat=True))
+        
+        #푸시 분산 발송용 리스트 생성
+        ios_registration_tokens = []
+        #한번에 푸시 발송 할 수량 설정 : kluck_env.env_settings.py
+        push_cnt = env.push_cnt
+        #푸시를 발송하는 단위 시간 : kluck_env.env_settings.py
+        push_term = env.push_term        
 
+        #전체 토큰을 발송량에 따라 분산 저장
+        for i in range(0, len(ios_registration_tokens_all), push_cnt):
+            temp = ios_registration_tokens_all[i:i+push_cnt]
+            ios_registration_tokens.append(temp)
+            
         # 오늘 날짜 가져오기
         today = datetime.now().strftime("%Y%m%d")
         # DB에서 오늘의 운세 메시지 가져오기
@@ -110,32 +122,37 @@ def send_push_ios():
             title = '오늘의 운세'
             body = '새벽 공기처럼 맑고 상쾌한 기운이 가득하길.🍃✨ 마음 가득 행복이 채워지는 날 되세요.🌷'
             push_logger.info(f"오늘의 운세 메시지가 존재하지 않습니다. today_luck_msg: {today_luck_msg} => 임의의 내용 작성: {body}")
-            
-        # 푸시 알림 (notification -> 백그라운드)
-        message = messaging.MulticastMessage( # 여러 기기에 메시지 전송
-            notification=messaging.Notification(
-                title=title,
-                body=body,
-            ),
-            # IOS 알림 설정 (APNs, Apple Push Notification Service)
-            apns=messaging.APNSConfig(
-                headers={
-                    'apns-push-type': 'background', # 알림 유형 == 백그라운드
-                    'apns-sound': 'default',
-                    'apns-priority': '10', # 알림 우선순위 == 높음(10)
-                },
-                payload=messaging.APNSPayload( # Android의 data 설정 역할 (alert으로 뜨는 정보)
-                    aps=messaging.Aps(
-                        content_available=True,
-                    ),
-                ),
-            ),
-            tokens = ios_registration_tokens, # 여러 개의 등록 토큰 리스트
-        )
 
-        # Firebase로 푸시 알림 전송
-        response = messaging.send_multicast(message)
-        push_logger.info(f"IOS 푸시 알림 발송 성공. Response: 'title' = {title}, 'body' = {body}")
+        
+        print(len(ios_registration_tokens_all))
+        print(len(ios_registration_tokens))
+        # for i in range(0, len(ios_registration_tokens)):
+        #     push_logger.info(f"Android 푸시용 리스트값: {ios_registration_tokens[i]}")
+        #     # 푸시 알림 (notification -> 백그라운드)
+        #     message = messaging.MulticastMessage( # 여러 기기에 메시지 전송
+        #         notification=messaging.Notification(
+        #             title=title,
+        #             body=body,
+        #         ),
+        #         # IOS 알림 설정 (APNs, Apple Push Notification Service)
+        #         apns=messaging.APNSConfig(
+        #             headers={
+        #                 'apns-push-type': 'background', # 알림 유형 == 백그라운드
+        #                 'apns-sound': 'default',
+        #                 'apns-priority': '10', # 알림 우선순위 == 높음(10)
+        #             },
+        #             payload=messaging.APNSPayload( # Android의 data 설정 역할 (alert으로 뜨는 정보)
+        #                 aps=messaging.Aps(
+        #                     content_available=True,
+        #                 ),
+        #             ),
+        #         ),
+        #         tokens = ios_registration_tokens, # 여러 개의 등록 토큰 리스트
+        #     )
+
+            # # Firebase로 푸시 알림 전송
+            # response = messaging.send_each_for_multicast(message)
+            # push_logger.info(f"IOS 푸시 알림 발송 성공. Response: 'title' = {title}, 'body' = {body}")
     
     except Exception as e:
         push_logger.error(f"IOS 푸시 알림 전송 중 오류 발생: {e}")
